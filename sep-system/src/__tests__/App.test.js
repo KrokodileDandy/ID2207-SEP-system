c729809984f1ce205a49a46b38dd6a8828e59f08
Sebastian Führ <s.fuehr1@gmail.com>
2022-10-11 18:00:28 +0300

    Minor changes

---
+++ sep-system/src/__tests__/App.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import App from '../App';
+
+test('renders the landing page', () => {
+  render(<App />);
+
+  expect(screen.getByRole("heading", {level: 1})).toHaveTextContent("SEP Container");
+});
