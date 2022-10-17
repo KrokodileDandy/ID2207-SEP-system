76a883cf54c4dbcf8f2784bec905118bf05c2882
Gard Aasness <gardaasness@gmail.com>
2022-10-17 15:13:17 +0200

    Add testing skeleton

--- sep-system/src/__tests__/auth.test.js
+++ sep-system/src/__tests__/auth.test.js
@@ -2,3 +2,11 @@
 test('sums numbers', () => {
     expect(1+2).toEqual(3);
 });
+
+beforeEach(() => {
+    render(<Login/>)
+});
+
+it("Navigate to new page when login is clicked with valid username and password", () => {
+    
+}) ;
