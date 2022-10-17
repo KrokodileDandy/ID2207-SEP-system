ceec662870d0b15565d7eb3d39eb7fff1abb6d79
Gard Aasness <gardaasness@gmail.com>
2022-10-17 14:33:36 +0200

    Change to functional component to not show on login page

--- sep-system/src/components/Navbar.js
+++ sep-system/src/components/Navbar.js
@@ -1,30 +1,29 @@
-import React from "react";
+import React, {useContext} from "react";
 import { Link } from 'react-router-dom';
+import useAuthenticateContext from "../context/useAuthenticate";
 
-class NavBar extends React.Component {
-  render() {
+function NavBar() {
+    const username  = useContext(useAuthenticateContext);
+    const list = ['ProductionManager', 'Employee', 'CustomerServiceManager'];
     return (
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/events">Events</Link>
-          </li>
-          <li>
-            <Link to="/tasks">Tasks</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-        </ul>
-      </nav>
+      list.includes(username.username) &&
+        <nav>
+          <ul>
+            <li>
+              <Link to="/home">Home</Link>
+            </li>
+            <li>
+              <Link to="/events">Events</Link>
+            </li>
+            <li>
+              <Link to="/tasks">Tasks</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
     )
   }
-}
 
 export default NavBar;
