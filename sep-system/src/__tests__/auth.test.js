4f459804ecf1f5f7f2b8c8dc3a73e22e40c88595
Gard Aasness <gardaasness@gmail.com>
2022-10-17 15:28:04 +0200

    Comment out unnecessary tests

--- sep-system/src/__tests__/auth.test.js
+++ sep-system/src/__tests__/auth.test.js
@@ -1,12 +1,12 @@
 // Needed in order to let tests pass. Remove once oyu have the first actual test up and running.
-test('sums numbers', () => {
-    expect(1+2).toEqual(3);
-});
+// test('sums numbers', () => {
+//     expect(1+2).toEqual(3);
+// });
 
-beforeEach(() => {
-    render(<Login/>)
-});
+// beforeEach(() => {
+//     render(<Login/>)
+// });
 
-it("Navigate to new page when login is clicked with valid username and password", () => {
+// it("Navigate to new page when login is clicked with valid username and password", () => {
     
-}) ;
+// }) ;
